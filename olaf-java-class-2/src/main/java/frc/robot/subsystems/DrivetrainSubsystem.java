1967c78bf965e44acf6b8a026420b64570545138
vwu1888 <vwu1888@gmail.com>
2022-11-19 15:28:54 -0500

    Day 4: Coded the drivetrain for Olaf

---
+++ olaf-java-class-2/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,39 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DrivetrainSubsystem extends SubsystemBase {
+    private WPI_TalonFX leftMotor1, leftMotor2, rightMotor1, rightMotor2;
+    private DifferentialDrive differentialDrive;
+
+    public DrivetrainSubsystem() {
+        leftMotor1 = new WPI_TalonFX(1);
+        leftMotor2 = new WPI_TalonFX(4);
+        rightMotor1 = new WPI_TalonFX(3);
+        rightMotor2 = new WPI_TalonFX(2);
+
+        differentialDrive = new DifferentialDrive(leftMotor1, rightMotor1);
+        differentialDrive.setDeadband(0.05);
+
+        leftMotor1.setInverted(true);
+        rightMotor1.setInverted(true);
+
+        leftMotor2.follow(leftMotor1);
+        leftMotor2.setInverted(InvertType.FollowMaster);
+        rightMotor2.follow(rightMotor1);
+        rightMotor2.setInverted(InvertType.FollowMaster);
+    }
+
+    public void tankDrive(double leftPower, double rightPower) {
+        differentialDrive.tankDrive(leftPower, rightPower);
+    }
+
+    public void stopDrive() {
+        leftMotor1.set(0.0);
+        rightMotor1.set(0.0);
+    }
+}
+
